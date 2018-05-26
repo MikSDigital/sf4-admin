ee98da3ead1b71753051ed19527c326387a411f1
vagrant <vagrant@homestead>
2018-05-26 10:59:12 +0000

    first commit

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ *
+ * @Route("/admin")
+ */
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="admin_index")
+     * @Method("GET")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="app_profile")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function profileAction(Request $request)
+    {
+        return $this->render('admin/pages/profile.html.twig', []);
+    }
+
+    /**
+     * @Route("/", name="app_dashboard")
+     *
+     * @param Request $request Request
+     *
+     * @return Response
+     */
+    public function dashboardAction(Request $request)
+    {
+        return $this->render('admin/pages/dashboard.html.twig', []);
+    }
+
+
+}
